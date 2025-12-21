c05a944e65374a1bf3d24f2c3383fc3e3e97289a
Muhammad Fakhri Azmar <fakhri.azmar12@gmail.com>
2025-12-21 18:32:42 +0700

    merapihkan fungsi yang ada

--- config/database.php
+++ config/database.php
@@ -1,5 +1,8 @@
 <?php
-session_start();
+// Start session if not already started
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 
 class Database {
     private $host = "localhost";
